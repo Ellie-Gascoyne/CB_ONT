4351c857a3d2b3c803355cdc4a8ab2f66937f3fb
Maurice Barrett <maurice@concert.bio>
2025-03-27 16:04:37 +0000

    update

--- src/bash_scripts/pixi_setup.sh
+++ src/bash_scripts/pixi_setup.sh
@@ -1,4 +1,13 @@
 pixi init
 
-pixi add --platform "osx-arm64" python=3.8
-pixi add --platform "linux-64" python=3.8
+pixi add --platform "osx-arm64" python=3.9
+pixi add --platform "linux-64" python=3.9
+
+# Add a channel to the environment
+pixi project channel add bioconda
+
+pixi add --platform "linux-64" "cutadapt==5.0"
+
+pixi add --platform "linux-64" "seqkit==2.10.0"
+
+pixi add --platform "linux-64" "chopper==0.9.2"
