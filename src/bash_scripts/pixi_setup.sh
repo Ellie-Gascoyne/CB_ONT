85325137f0495493aee9ecceebf9891835873cda
Maurice Barrett <maurice@concert.bio>
2025-05-29 16:37:38 +0100

    update

--- src/bash_scripts/pixi_setup.sh
+++ src/bash_scripts/pixi_setup.sh
@@ -6,20 +6,26 @@ pixi add --platform "linux-64" python=3.9
 # Add a channel to the environment
 pixi project channel add bioconda
 
+# Maurice to add to Linux machine
+
 pixi add --platform "linux-64" "cutadapt==5.0"
 
 pixi add --platform "linux-64" "seqkit==2.10.0"
 
 pixi add --platform "linux-64" "chopper==0.9.2"
 
-pixi add --platform "linux-64" "emu==3.5.1"
+pixi add --platform "linux-64" "grepq"
 
-pixi remove --platform "linux-64" "cutadapt"
+pixi add --platform "linux-64" "r-optparse"
 
+pixi add --platform "linux-64" bioconductor-decipher
 
+pixi add --platform "linux-64" "picrust2==2.6.2"
 
-pixi remove --platform "osx-arm64"  chooper 
+# Ellie to add to Mac machine
+pixi add --platform "osx-arm64" "bioconductor-decipher"
 
+pixi add --platform "osx-arm64" "picrust2==2.6.2"
 
-
-pixi add --platform "osx-arm64"  "emu==3.5.1"
+# Ellie to remove from Mac machine
+pixi remove --platform "osx-arm64" emu
